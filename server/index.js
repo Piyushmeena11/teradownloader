4e8b81056f0e6e00262abb48b7c3fbd07b3cae1c
Piyush <meenapiyush23@gmail.com>
2025-12-26 23:35:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,10 +10,10 @@ import axios from 'axios';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3002;
